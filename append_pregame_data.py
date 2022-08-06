43d45e0676896a1292301d4214d5bcf675abbe88
LucasPierru <lucaspierru7@gmail.com>
2022-08-06 12:16:27 -0400

    get best players at date and game players

--- append_pregame_data.py
+++ append_pregame_data.py
@@ -1,13 +1,43 @@
+from distutils.command import check
 import pandas as pd
 
 player_df = pd.read_excel('player_data_2021-22.xlsx')
 
-def get_team_best_players(df, game_id, team_id):
-  game_df = df[df['GAME_ID'] == game_id]
-  team_game_df = game_df[game_df['TEAM_ID'] == team_id]
+def get_team_current_players_stats(player_df, team_id, date, n_best_players=3):
+  """
+  Function that allows you to get the best nba players
+  of a team at a certain date with the player dataframe
+  you can also select a certain amount of players
 
-  print(team_game_df)
+  Parameters:
+  - player_df: Players dataframe
+  - team_id: id of the team of which you want the best players
+  - date: the date at which you want the data
+  - n_best_players: the numbers of players you want
 
-get_team_best_players(player_df, 22100082, 1610612737)
+  Return:
+  - A dataframe of the best players of selected team
+  """
+  # game_df = df[df['GAME_ID'] == game_id]
+  team_df = player_df[player_df['TEAM_ID'] == team_id]
+  team_df['GAME_DATE'] = team_df['GAME_DATE'].astype(str).str[:10]
+  team_at_date_df = team_df[team_df['GAME_DATE'] < date]
+  team_at_date_df = team_at_date_df.sort_values(by=['GAME_DATE', 'PLAYER_NAME'], ascending=False)
+  team_at_date_df = team_at_date_df.drop_duplicates(subset=['PLAYER_NAME'], keep='first')
 
-# print(player_df)
+  team_at_date_df = team_at_date_df.sort_values(by=['PLAYER_PER'], ascending=False)
+  
+  return team_at_date_df[:n_best_players]
+
+def check_best_player_in_game(player_df, game_id, team_id):
+  game_df = player_df[player_df['GAME_ID'] == game_id]
+  team_df = game_df[game_df['TEAM_ID'] == team_id]
+
+  return team_df
+
+df = get_team_current_players_stats(player_df, 1610612737, '2021-12-03', 5)
+print(df)
+print(df['GAME_DATE'])
+
+df2 = check_best_player_in_game(player_df, 22100335, 1610612737)
+print(df2)
